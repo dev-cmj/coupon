04218e456f618a79575da89b8c8b14519cdb41ac
dungh <dunghasd@gmail.com>
2024-06-14 21:02:23 +0900

    부하테스트 1 : TPS
    서버에서 동시에 처리할 수 있는 수 (톰캣의 경우 기본 200) x N (Thread 0.5 이므로 2) = 400
    기본적인 Transaction Per Second 에 대해 알게 되었다.

--- coupon-api/src/main/java/com/cmj/couponapi/controller/HelloController.java
+++ coupon-api/src/main/java/com/cmj/couponapi/controller/HelloController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello() throws InterruptedException {
+        Thread.sleep(500);
         return "Hello, World!";
-    }
+    } // 초당 2건을 처리 * N (200) = 400 (서버에서 동시에 처리할 수 있는 수) = TPS (Transaction Per Second)
 }
