30710b65334bb13aae27b0a746f17e03c1eb3901
dungh <dunghasd@gmail.com>
2024-06-15 15:37:05 +0900

    쿠폰 엔티티 발급 기능 테스트 코드 추가
    쿠폰 도메인 내부에 유효성 검사와 발급 관련된 기능을 테스트 하는 코드 작성

---
+++ coupon-core/src/main/java/com/cmj/couponcore/exception/CouponIssueException.java
@@ -0,0 +1,17 @@
+package com.cmj.couponcore.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CouponIssueException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+    private final String message;
+
+    @Override
+    public String getMessage() {
+        return "[%s] %s".formatted(errorCode, message);
+    }
+}
---
+++ coupon-core/src/main/java/com/cmj/couponcore/exception/ErrorCode.java
@@ -0,0 +1,8 @@
+package com.cmj.couponcore.exception;
+
+public enum ErrorCode {
+    INVALID_COUPON_ISSUE_QUANTITY,
+
+    INVALID_COUPON_ISSUE_DATE,
+    ;
+}
