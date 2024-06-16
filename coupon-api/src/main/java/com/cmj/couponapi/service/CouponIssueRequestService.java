45de6bcac7a385dbd4b31ad24a902c20677305a6
dungh <dunghasd@gmail.com>
2024-06-16 17:25:56 +0900

    Redis Client Redisson을 통한 동시성 락 제어
    레디스 클라이언트인 레디슨을 사용해 동시성 제어를 구현했다.

--- coupon-api/src/main/java/com/cmj/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/cmj/couponapi/service/CouponIssueRequestService.java
@@ -1,6 +1,7 @@
 package com.cmj.couponapi.service;
 
 import com.cmj.couponapi.controller.dto.CouponIssueRequestDto;
+import com.cmj.couponcore.component.DistributeLockExecutor;
 import com.cmj.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,12 +13,12 @@ import org.springframework.stereotype.Service;
 public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
+    private final DistributeLockExecutor distributeLockExecutor;
 
 
     public void issueRequestV1(CouponIssueRequestDto couponIssueRequestDto) {
-        synchronized (this) {
-            couponIssueService.issue(couponIssueRequestDto.couponId(), couponIssueRequestDto.userId());
-        }
+        distributeLockExecutor.execute("lock_" + couponIssueRequestDto.couponId(), 10000, 10000,
+                () -> couponIssueService.issue(couponIssueRequestDto.couponId(), couponIssueRequestDto.userId()));
         log.info("쿠폰 발급 요청 완료. userId: {}, couponId: {}", couponIssueRequestDto.userId(), couponIssueRequestDto.couponId());
     }
 }
