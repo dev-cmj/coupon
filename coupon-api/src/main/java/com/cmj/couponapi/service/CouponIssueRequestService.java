3549aa826c5ad515c12865c6828536a6469feb04
dungh <dunghasd@gmail.com>
2024-06-17 20:08:16 +0900

    쿠폰 발급 요청 API 개발 - sorted set 사용
    sorted set을 사용하면 어떤 문제점이 발생하는지 알기 위해 적용 시켜보기

--- coupon-api/src/main/java/com/cmj/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/cmj/couponapi/service/CouponIssueRequestService.java
@@ -2,6 +2,7 @@ package com.cmj.couponapi.service;
 
 import com.cmj.couponapi.controller.dto.CouponIssueRequestDto;
 import com.cmj.couponcore.component.DistributeLockExecutor;
+import com.cmj.couponcore.service.AsyncCouponIssueServiceV1;
 import com.cmj.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,11 +14,16 @@ import org.springframework.stereotype.Service;
 public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
+    private final AsyncCouponIssueServiceV1 asyncCouponIssueServiceV1;
     private final DistributeLockExecutor distributeLockExecutor;
 
 
     public void issueRequestV1(CouponIssueRequestDto couponIssueRequestDto) {
         couponIssueService.issue(couponIssueRequestDto.couponId(), couponIssueRequestDto.userId());
         log.info("쿠폰 발급 요청 완료. userId: {}, couponId: {}", couponIssueRequestDto.userId(), couponIssueRequestDto.couponId());
+    }
+
+    public void asyncIssueRequestV1(CouponIssueRequestDto couponIssueRequestDto) {
+        asyncCouponIssueServiceV1.issue(couponIssueRequestDto.couponId(), couponIssueRequestDto.userId());
     }
 }
