6d41387e4e8457fae62e217e6d276ded60b2cc0f
dungh <dunghasd@gmail.com>
2024-06-19 22:00:20 +0900

    쿠폰 발급 요청 API 개발 - script
    사용중이던 DistributeLockExecutor는 Redis의 락을 제대로 활성화하지 못했던 문제점을 발견했다.
    Redis의 script을 통해 쿠폰 발급 로직을 하나로 묶어 원자성을 갖게 하여서 동시성 제어를 해결했다.

--- coupon-api/src/main/java/com/cmj/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/cmj/couponapi/service/CouponIssueRequestService.java
@@ -3,6 +3,7 @@ package com.cmj.couponapi.service;
 import com.cmj.couponapi.controller.dto.CouponIssueRequestDto;
 import com.cmj.couponcore.component.DistributeLockExecutor;
 import com.cmj.couponcore.service.AsyncCouponIssueServiceV1;
+import com.cmj.couponcore.service.AsyncCouponIssueServiceV2;
 import com.cmj.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +16,7 @@ public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
     private final AsyncCouponIssueServiceV1 asyncCouponIssueServiceV1;
-    private final DistributeLockExecutor distributeLockExecutor;
+    private final AsyncCouponIssueServiceV2 asyncCouponIssueServiceV2;
 
 
     public void issueRequestV1(CouponIssueRequestDto couponIssueRequestDto) {
@@ -26,4 +27,10 @@ public class CouponIssueRequestService {
     public void asyncIssueRequestV1(CouponIssueRequestDto couponIssueRequestDto) {
         asyncCouponIssueServiceV1.issue(couponIssueRequestDto.couponId(), couponIssueRequestDto.userId());
     }
+
+    public void asyncIssueRequestV2(CouponIssueRequestDto couponIssueRequestDto) {
+        asyncCouponIssueServiceV2.issue(couponIssueRequestDto.couponId(), couponIssueRequestDto.userId());
+    }
+
+
 }
