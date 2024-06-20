0870c49b3288b4403b6d50d01a0999ed50af1e5d
dungh <dunghasd@gmail.com>
2024-06-20 21:05:10 +0900

    쿠폰 발급 기능 테스트 및 로컬 캐시 적용
    로컬 캐시와 이벤트 리스너를 통해 레디스의 부하를 줄이기

--- coupon-core/src/main/java/com/cmj/couponcore/service/CouponIssueService.java
+++ coupon-core/src/main/java/com/cmj/couponcore/service/CouponIssueService.java
@@ -4,10 +4,12 @@ package com.cmj.couponcore.service;
 import com.cmj.couponcore.exception.CouponIssueException;
 import com.cmj.couponcore.model.Coupon;
 import com.cmj.couponcore.model.CouponIssue;
+import com.cmj.couponcore.model.event.CouponIssueCompleteEvent;
 import com.cmj.couponcore.repository.mysql.CouponIssueJpaRepository;
 import com.cmj.couponcore.repository.mysql.CouponIssueRepository;
 import com.cmj.couponcore.repository.mysql.CouponJpaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,11 +24,13 @@ public class CouponIssueService {
     private final CouponJpaRepository couponJpaRepository;
     private final CouponIssueJpaRepository couponIssueJpaRepository;
     private final CouponIssueRepository couponIssueRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     public void issue(long couponId, long userId) {
             Coupon coupon = findCouponWithLock(couponId);
             coupon.issue();
             saveCouponIssue(couponId, userId);
+            publishCouponEvent(coupon);
     }
 
     public Coupon findCouponWithLock(long couponId) {
@@ -45,6 +49,12 @@ public class CouponIssueService {
     private void checkAlreadyIssuance(long couponId, long userId) {
         if (couponIssueRepository.findFirstCouponIssue(couponId, userId).isPresent()) {
             throw new CouponIssueException(DUPLICATED_COUPON_ISSUE, "이미 발급된 쿠폰입니다. userId: %s, couponId: %s".formatted(userId, couponId));
+        }
+    }
+
+    private void publishCouponEvent(Coupon coupon) {
+        if (coupon.isIssueComplete()) {
+            applicationEventPublisher.publishEvent(new CouponIssueCompleteEvent(coupon.getId()));
         }
     }
 
